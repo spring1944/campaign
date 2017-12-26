f299641675c774d9a9dfed766584119102187628
Jose Luis Cercos-Pita <jlcercos@gmail.com>
2017-12-26 14:43:30 +0100

    Replace Tabs by Spaces

--- menu.sdd/modinfo.lua
+++ menu.sdd/modinfo.lua
@@ -1,23 +1,23 @@
 -- Authors: Jose Luis Cercos-Pita
 
 local modinfo = {
-	name            = "Spring:1944-Campaign",
-	shortName       = "S44-Campaign",
-	version         = "",
-	game            = "S44-Campaign",
-	shortGame       = "S44-Campaign",
-	mutator         = "Official",
-	description     = "Spring:1944 campaign lobby",
-	modtype         = "5",
-	url             = "https://gitlab.com/Spring1944/campaign",
-	depend = {
-		"Spring Cursors",
-		"Spring content v1",
-		"Spring:1944-CampaignAPI",
-		"S44-Poland 1.0",
-	},
-	onlyLocal       = true,
-	engine          = "104",
+    name            = "Spring:1944-Campaign",
+    shortName       = "S44-Campaign",
+    version         = "",
+    game            = "S44-Campaign",
+    shortGame       = "S44-Campaign",
+    mutator         = "Official",
+    description     = "Spring:1944 campaign lobby",
+    modtype         = "5",
+    url             = "https://gitlab.com/Spring1944/campaign",
+    depend = {
+        "Spring Cursors",
+        "Spring content v1",
+        -- Required packages
+        "S44-Poland 1.0",
+    },
+    onlyLocal       = true,
+    engine          = "104",
 }
 
 return modinfo 
