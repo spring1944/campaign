f315af361f93fb4cbc431bcba7ce75e5befcc71f
Yuriy Chertkov <s44yuritch@gmail.com>
2018-01-09 13:05:23 +0200

    Made Poland sdd selectable as a game (for testing)

--- poland.sdd/modinfo.lua
+++ poland.sdd/modinfo.lua
@@ -27,7 +27,7 @@ return {
     --      for single units for example. 
     --  others - perhaps mutators and addon races that can be 
     --           enabled in addition to xta for example? 
-    modtype = "0",
+    modtype = "1",
     -- Number of other archives this one depends on 
     depend= {
         "Spring: 1944 $VERSION",
