2246ef6bdc2ac45350969dff66fa9118b2209162
Yuriy Chertkov <s44yuritch@gmail.com>
2018-02-12 14:38:33 +0200

    Polish scout plane

---
+++ poland.sdd/units/pl/aircraft/PLRWD8.lua
@@ -0,0 +1,16 @@
+local PLRWD8 = Recon:New{
+	name			= "RWD 8",
+	maxDamage		= 75,
+	
+	maxAcc			= 0.480,
+	
+	customParams = {
+		enginesound		= "po2-",
+		enginesoundnr	= 11,
+	},
+}
+
+
+return lowerkeys({
+	["PLRWD8"] = PLRWD8,
+})
