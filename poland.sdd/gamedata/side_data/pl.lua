9aa805c60056d0065ff8fce10bdcd7797944d701
Jose Luis Cercos-Pita <jlcercos@gmail.com>
2018-02-28 15:10:02 +0100

    Addapted Poland to the new factions generation methodology

---
+++ poland.sdd/gamedata/side_data/pl.lua
@@ -0,0 +1,23 @@
+local pldata = {
+    name = "PL",
+    startUnit = "PLHQEngineer",
+    wiki_title = "Poland",
+    wiki_desc = [[## Introduction
+
+Poland was invaded by Germany and Soviet Union, almost at the same time (the
+former began the hostilities on 1 September 1939 while the later waited up to
+17 September 1939. However, the relationship with both invaders was extremely
+different. While German forces caused 5.7 million casualties, Soviet Union
+caused 150000 deads. Such difference leaded Poland to join Soviet Union at
+further stages of the war, even getting his own army, aknowledged by Stalin.
+
+## Strengths
+
+.
+
+## Weakness
+
+.]],
+} 
+
+return pldata
